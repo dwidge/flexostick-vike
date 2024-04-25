61eeff574d399f6b09e03dc457ad2c36098eb72d
dwidge <dwidge+gh@gmail.com>
2024-04-25 23:06:59 +0200

    Update banner

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -1,65 +1,37 @@
-import React, { useState } from "react";
-// import "../../styles/reset.css";
-// import "../../styles/home.css";
 import { config } from "../config.js";
 import { Link } from "../../renderer/Link.js";
 import banner from "../../images/banner.png";
 import { Home } from "./Home.js";
 
 export function Page() {
-  // const submitted = useState<boolean>(false);
-
   return (
     <div className="flex column wide shadow">
-      {/* <img src={banner} /> */}
+      <img src={banner} />
       <section
-        className="flex0 row spaced wrap dark pad"
+        className="flex row spaced wrap semidark pad gap"
         style={{
-          backgroundImage: `url(${banner})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          backgroundPosition: "bottom right",
+          justifyContent: "space-between",
         }}
       >
-        <section
-          className="flex column"
-          style={{
-            height: "14em",
-            justifyContent: "flex-end",
-            alignContent: "flex-end",
-          }}
-        >
-          {/* <section className="logo">
-            <div style={{ display: "grid", gridAutoColumns: "2fr" }}>
-              <div>g</div>
-              <div style={{ flex: 1 }}>
-                <h2>
-                  <Link href="/">{config.app.title}</Link>
-                </h2>
-                Label Manufacturer
-              </div>
-            </div>
-          </section> */}
-          <section className="navigation">
-            <nav
-              role="navigation"
-              className="row gap wrap"
-              style={{ justifyContent: "flex-end" }}
-            >
-              <Link className="button" href="/">
-                What We Do
-              </Link>
-              <Link className="button" href="/">
-                Request a Quote
-              </Link>
-            </nav>
-          </section>
+        <section className="navigation">
+          <nav
+            role="navigation"
+            className="row gap wrap"
+            style={{ justifyContent: "flex-end" }}
+          >
+            <Link className="button" href="/">
+              What We Do
+            </Link>
+            <Link className="button" href="/">
+              Request a Quote
+            </Link>
+          </nav>
         </section>
       </section>
       <div className="flex dark">
         <Home />
       </div>
-      <section className="flex0 semidark pad right">
+      <section className="auto row semidark pad gap left">
         <p>
           We are a Level 4 BEE contributor and Essential Supplier. © 2023{" "}
           {config.app.title}
