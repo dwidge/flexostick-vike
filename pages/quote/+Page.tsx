afbea3a3ef0b454dda2c55180912b0665e74fe1a
dwidge <dwidge+gh@gmail.com>
2024-04-30 13:12:41 +0200

    Customize quote page

--- pages/quote/+Page.tsx
+++ pages/quote/+Page.tsx
@@ -3,7 +3,6 @@ import { QuoteForm } from "./QuoteForm";
 export function Page() {
   return (
     <div className="flex semidark pad">
-      <h3>Request a quote</h3>
       <QuoteForm />
     </div>
   );
--- pages/quote/Quote.ts
+++ pages/quote/Quote.ts
@@ -4,6 +4,9 @@ import { phoneRegex } from "../../utils/phoneRegex";
 export const Quote = z.object({
   name: z.string({ message: "Name required" }),
   phone: z.string().regex(phoneRegex, "Phone invalid").optional(),
+  labelSize: z.string().optional(),
+  labelMaterial: z.string().optional(),
+  quantity: z.number().int().multipleOf(1000).min(1000).optional(),
   message: z.string().optional(),
 });
 export type Quote = z.infer<typeof Quote>;
